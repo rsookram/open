847605ca0e8d43d7213ade9df0461dbf1801b6bd
rsookram <rashadsookram@gmail.com>
2022-09-18 16:48:02 -0400

    Add project skeleton

---
+++ src/main/java/io/github/rsookram/open/MainActivity.java
@@ -0,0 +1,12 @@
+package io.github.rsookram.open;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
